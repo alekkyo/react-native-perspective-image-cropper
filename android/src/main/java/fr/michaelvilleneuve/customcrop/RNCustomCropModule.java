ddb779f845ad72860b6fb0c123c6ded20d6f4236
Alex Kua <alexkua@me.com>
2019-12-03 21:07:14 -0500

    Fix cropping ratio

--- android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
+++ android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
@@ -43,69 +43,92 @@ import java.util.List;
 
 public class RNCustomCropModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNCustomCropModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNCustomCropModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "CustomCropManager";
-  }
+    @Override
+    public String getName() {
+        return "CustomCropManager";
+    }
 
-  @ReactMethod
-  public void crop(ReadableMap points, String imageUri, Callback callback) {
+    @ReactMethod
+    public void crop(ReadableMap points, String imageUri, Callback callback) {
+        // Get coordinates from parameter
+        Point tl = new Point(points.getMap("topLeft").getDouble("x"), points.getMap("topLeft").getDouble("y"));
+        Point tr = new Point(points.getMap("topRight").getDouble("x"), points.getMap("topRight").getDouble("y"));
+        Point bl = new Point(points.getMap("bottomLeft").getDouble("x"), points.getMap("bottomLeft").getDouble("y"));
+        Point br = new Point(points.getMap("bottomRight").getDouble("x"), points.getMap("bottomRight").getDouble("y"));
+        double appWidth = points.getDouble("width");
+        double appHeight = points.getDouble("height");
 
-    Point tl = new Point(points.getMap("topLeft").getDouble("x"), points.getMap("topLeft").getDouble("y"));
-    Point tr = new Point(points.getMap("topRight").getDouble("x"), points.getMap("topRight").getDouble("y"));
-    Point bl = new Point(points.getMap("bottomLeft").getDouble("x"), points.getMap("bottomLeft").getDouble("y"));
-    Point br = new Point(points.getMap("bottomRight").getDouble("x"), points.getMap("bottomRight").getDouble("y"));
+        // Get image from url
+        Mat src = Imgcodecs.imread(imageUri.replace("file://", ""), Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2RGB);
 
-    Mat src = Imgcodecs.imread(imageUri.replace("file://", ""), Imgproc.COLOR_BGR2RGB);
-    Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2RGB);
+        //boolean ratioAlreadyApplied = tr.x * (src.size().width / 500) < src.size().width;
+        //double ratio = ratioAlreadyApplied ? src.size().width / 500 : 1;
+        //double ratio = src.size().width / 500;
+        double widthRatio = src.size().width / appWidth;
+        double heightRatio = src.size().height / appHeight;
 
-    boolean ratioAlreadyApplied = tr.x * (src.size().width / 500) < src.size().width;
-    double ratio = ratioAlreadyApplied ? src.size().width / 500 : 1;
+        double widthA = Math.sqrt(Math.pow(br.x - bl.x, 2) + Math.pow(br.y - bl.y, 2));
+        double widthB = Math.sqrt(Math.pow(tr.x - tl.x, 2) + Math.pow(tr.y - tl.y, 2));
 
-    double widthA = Math.sqrt(Math.pow(br.x - bl.x, 2) + Math.pow(br.y - bl.y, 2));
-    double widthB = Math.sqrt(Math.pow(tr.x - tl.x, 2) + Math.pow(tr.y - tl.y, 2));
+        double dw = Math.max(widthA, widthB) * widthRatio;
+        int maxWidth = Double.valueOf(dw).intValue();
 
-    double dw = Math.max(widthA, widthB) * ratio;
-    int maxWidth = Double.valueOf(dw).intValue();
+        double heightA = Math.sqrt(Math.pow(tr.x - br.x, 2) + Math.pow(tr.y - br.y, 2));
+        double heightB = Math.sqrt(Math.pow(tl.x - bl.x, 2) + Math.pow(tl.y - bl.y, 2));
 
-    double heightA = Math.sqrt(Math.pow(tr.x - br.x, 2) + Math.pow(tr.y - br.y, 2));
-    double heightB = Math.sqrt(Math.pow(tl.x - bl.x, 2) + Math.pow(tl.y - bl.y, 2));
+        double dh = Math.max(heightA, heightB) * heightRatio;
+        int maxHeight = Double.valueOf(dh).intValue();
 
-    double dh = Math.max(heightA, heightB) * ratio;
-    int maxHeight = Double.valueOf(dh).intValue();
+        Mat doc = new Mat(maxHeight, maxWidth, CvType.CV_8UC4);
 
-    Mat doc = new Mat(maxHeight, maxWidth, CvType.CV_8UC4);
+        Mat src_mat = new Mat(4, 1, CvType.CV_32FC2);
+        Mat dst_mat = new Mat(4, 1, CvType.CV_32FC2);
 
-    Mat src_mat = new Mat(4, 1, CvType.CV_32FC2);
-    Mat dst_mat = new Mat(4, 1, CvType.CV_32FC2);
+        src_mat.put(
+                0,
+                0,
+                tl.x * widthRatio,
+                tl.y * heightRatio,
+                tr.x * widthRatio,
+                tr.y * heightRatio,
+                br.x * widthRatio,
+                br.y * heightRatio,
+                bl.x * widthRatio,
+                bl.y * heightRatio
+        );
+        dst_mat.put(0, 0, 0.0, 0.0, dw, 0.0, dw, dh, 0.0, dh);
 
-    src_mat.put(0, 0, tl.x * ratio, tl.y * ratio, tr.x * ratio, tr.y * ratio, br.x * ratio, br.y * ratio, bl.x * ratio,
-        bl.y * ratio);
-    dst_mat.put(0, 0, 0.0, 0.0, dw, 0.0, dw, dh, 0.0, dh);
+        Mat m = Imgproc.getPerspectiveTransform(src_mat, dst_mat);
 
-    Mat m = Imgproc.getPerspectiveTransform(src_mat, dst_mat);
+        Imgproc.warpPerspective(src, doc, m, doc.size());
 
-    Imgproc.warpPerspective(src, doc, m, doc.size());
+        Bitmap bitmap = Bitmap.createBitmap(doc.cols(), doc.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(doc, bitmap);
 
-    Bitmap bitmap = Bitmap.createBitmap(doc.cols(), doc.rows(), Bitmap.Config.ARGB_8888);
-    Utils.matToBitmap(doc, bitmap);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
 
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
-    byte[] byteArray = byteArrayOutputStream.toByteArray();
+        WritableMap map = Arguments.createMap();
+        map.putString("image", Base64.encodeToString(byteArray, Base64.DEFAULT));
+        map.putString("widthRatio", String.valueOf(widthRatio));
+        map.putString("heightRatio", String.valueOf(heightRatio));
+        map.putString("imageWidth", String.valueOf(src.size().width));
+        map.putString("imageHeight", String.valueOf(src.size().height));
+        map.putString("widthA", String.valueOf(widthA));
+        map.putString("widthB", String.valueOf(widthB));
+        map.putString("heightA", String.valueOf(heightA));
+        map.putString("heightB", String.valueOf(heightB));
+        callback.invoke(null, map);
 
-    WritableMap map = Arguments.createMap();
-    map.putString("image", Base64.encodeToString(byteArray, Base64.DEFAULT));
-    callback.invoke(null, map);
-
-    m.release();
-  }
-
+        m.release();
+    }
 }
